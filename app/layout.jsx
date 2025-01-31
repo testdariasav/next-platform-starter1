f99da0fb13a3ce438365de9292e380b9d853b818
testdariasav <testdariasav@gmail.com>
2025-01-31 09:23:23 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
             <script>
 
                 function handler(){
-                    console.log("hover");
+                    console.log("hover")
 
                 }
                 
